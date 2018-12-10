8e9c6e83049956a9497f12a1489bb8e64c250ac2
Filiberto Reyes <filibertoreyes55@gmail.com>
2018-12-09 22:06:17 -0800

    added modal

--- src/views/Level1.vue
+++ src/views/Level1.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <v-card height="200px" flat>
     <div class="headline text-xs-center pa-5">
       Active: {{ bottomNav }}
@@ -37,13 +38,58 @@
       </v-btn>
     </v-bottom-nav>
   </v-card>
+
+  <div class="text-xs-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <v-btn
+        slot="activator"
+        color="red lighten-2"
+        dark
+      >
+        Modal
+      </v-btn>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Who is Orpheus?
+        </v-card-title>
+
+        <v-card-text>
+          A couragous young man equiped with the harp of hearts destined for a legendary journey. <br><br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</div>
+  
 </template>
 
 <script>
   export default {
     data () {
       return {
-        bottomNav: 'recent'
+        bottomNav: 'recent',
+        dialog: false
       }
     }
   }
