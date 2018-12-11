bca7e26a02388d2ea3ad91b7b262142535480c0e
Dustin Lahr <dustin.lahr.367@my.csun.edu>
2018-12-10 19:02:15 -0800

    working modals

--- src/views/Level1.vue
+++ src/views/Level1.vue
@@ -1,68 +1,28 @@
 <template>
-<div>
-  <v-card height="200px" flat>
-    <div class="headline text-xs-center pa-5">
-      Active: {{ bottomNav }}
-    </div>
-    <v-bottom-nav
-      :active.sync="bottomNav"
-      :value="true"
-      absolute
-      color="transparent"
-    >
-      <v-btn
-        color="teal"
-        flat
-        value="Skills"
-      >
-        <span>Skills</span>
-        <v-icon>star</v-icon>
-      </v-btn>
-
-      <v-btn
-        color="teal"
-        flat
-        value="Map"
-      >
-        <span>Map</span>
-        <v-icon>map</v-icon>
-      </v-btn>
-
-      <v-btn
-        color="teal"
-        flat
-        value="Options"
-      >
-        <span>Options</span>
-        <v-icon>settings</v-icon>
-      </v-btn>
-    </v-bottom-nav>
-  </v-card>
-
-  <div class="text-xs-center">
+<div style="display: inline-block;">
+  <!-- Skills -->
     <v-dialog
       v-model="dialog"
       width="500"
     >
       <v-btn
+        color="teal"
+        flat
         slot="activator"
-        color="red lighten-2"
-        dark
       >
-        Modal
+        <span>Skills</span>
+        <v-icon>star</v-icon>
       </v-btn>
-
       <v-card>
         <v-card-title
           class="headline grey lighten-2"
           primary-title
         >
-          Who is Orpheus?
+          Options
         </v-card-title>
 
         <v-card-text>
-          A couragous young man equiped with the harp of hearts destined for a legendary journey. <br><br>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+        MAP INFORMATION
         </v-card-text>
 
         <v-divider></v-divider>
@@ -79,12 +39,92 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-  </div>
+
+      <!-- map -->
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <v-btn
+        color="teal"
+        flat
+        slot="activator"
+      >
+        <span>Map</span>
+        <v-icon>map</v-icon>
+      </v-btn>
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Options
+        </v-card-title>
+
+        <v-card-text>
+        MAP INFORMATION
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <v-btn
+        slot="activator"
+        color="teal"
+        flat
+      >
+        <span>Options</span>
+        <v-icon>settings</v-icon>
+      </v-btn>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Options
+        </v-card-title>
+
+        <v-card-text>
+        OPTIONS
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 </div>
-  
+
 </template>
 
 <script>
+
   export default {
     data () {
       return {
