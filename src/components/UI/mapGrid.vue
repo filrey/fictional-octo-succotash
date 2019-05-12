95061a63b8810c27363fb7c9706bd3061b49f37f
Filiberto Reyes <filibertoreyes55@gmail.com>
2019-05-12 00:27:00 -0700

    rows and columns for game space

--- src/components/UI/mapGrid.vue
+++ src/components/UI/mapGrid.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        This is the map grid component
+        <v-container>
+            <v-layout row wrap v-for="j in 9" :key="j">
+                <v-flex v-for="n in 10" :key="n">
+                    <v-card height="100px" width="calc(100%)">
+                        Row {{j}}
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 
