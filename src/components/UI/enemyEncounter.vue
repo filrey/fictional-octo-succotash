9aecdb588fea620cdd758a07f1f1bd90e2aecd8d
Filiberto Reyes <filibertoreyes55@gmail.com>
2019-05-21 15:15:13 -0700

    place holders for avatars

--- src/components/UI/enemyEncounter.vue
+++ src/components/UI/enemyEncounter.vue
@@ -7,7 +7,28 @@
 
         <!-- <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text> -->
 
-        <!-- <v-divider></v-divider> -->
+        <div>
+          <img
+            class="avatarOne"
+            src="https://vuetifyjs.com/apple-touch-icon-180x180.png"
+            alt="avatar"
+          >
+          <img
+            class="avatarTwo"
+            src="https://vuetifyjs.com/apple-touch-icon-180x180.png"
+            alt="avatar"
+          >
+          <img
+            class="avatarThree"
+            src="https://vuetifyjs.com/apple-touch-icon-180x180.png"
+            alt="avatar"
+          >
+          <img
+            class="avatarFour"
+            src="https://vuetifyjs.com/apple-touch-icon-180x180.png"
+            alt="avatar"
+          >
+        </div>
 
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -19,26 +40,52 @@
 </template>
 
 <script>
+import orpheus from "@/assets/images/orpheus_idle.png";
 export default {
   name: "enemyEncounter",
   data() {
     return {
-      dialog: true
+      dialog: true,
+      orpheus: orpheus
     };
   }
 };
 </script>
 
 <style lang="css">
-    .gridBG{
-     background-image: url('../../assets/images/environment_forestbackground.png');
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-     background-size: cover;
-    }
+.gridBG {
+  background-image: url("../../assets/images/environment_forestbackground.png");
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
 
-    .encounterBoxHeight{
-        height: 600px;
-    }
+.encounterBoxHeight {
+  height: 600px;
+}
+
+.avatarOne {
+  position: absolute;
+  left: 50px;
+  top: 200px;
+}
+
+.avatarTwo {
+  position: absolute;
+  right: 50px;
+  top: 100px;
+}
+
+.avatarThree {
+  position: absolute;
+  right: 50px;
+  top: 200px;
+}
+
+.avatarFour {
+  position: absolute;
+  right: 50px;
+  top: 300px;
+}
 </style>
