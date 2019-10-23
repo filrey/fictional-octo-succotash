77c69fa5d19eabd037001a3d37eed03b49e3be21
Filiberto Reyes <filibertoreyes55@gmail.com>
2019-10-23 12:26:35 -0700

    adding dist

---
+++ dist/precache-manifest.5fec00ce9ef083ff83388b2c8100ae07.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3438370c40d1351377ae38f1c1e3daf",
+    "url": "/img/orpheus.a3438370.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bfdc5b298df7d8d10948a306f85502ef",
+    "url": "/media/Explosion1.bfdc5b29.mp3"
+  },
+  {
+    "revision": "e5233176773ba51e3417",
+    "url": "/js/app.0fd61a37.js"
+  },
+  {
+    "revision": "22a3b74b4aa635d966f4",
+    "url": "/js/chunk-vendors.a81f6dc2.js"
+  },
+  {
+    "revision": "eb9e6315809acc559eb0cf05e8c77cc9",
+    "url": "/media/howl.eb9e6315.mp3"
+  },
+  {
+    "revision": "cdb4f72489c04d6255aba76a4b11d2ee",
+    "url": "/media/Netherplace_Looping.cdb4f724.mp3"
+  },
+  {
+    "revision": "70227f5241fad02d2f7b77d85882361c",
+    "url": "/media/oldcrypt.70227f52.mp3"
+  },
+  {
+    "revision": "bae66c8ad3f01c522fab9be089693fef",
+    "url": "/media/Our-Mountain_v003_Looping.bae66c8a.mp3"
+  },
+  {
+    "revision": "11dbf9c6fd87cb0134ef15df86d41d89",
+    "url": "/media/Fantasy-Forest-Battle.11dbf9c6.mp3"
+  },
+  {
+    "revision": "6d1a5f1f567c9e71b2a50f9095a218c7",
+    "url": "/media/dragon.6d1a5f1f.mp3"
+  },
+  {
+    "revision": "ed5d9c9d739b60b0ecc0930441b38572",
+    "url": "/media/flute2.ed5d9c9d.mp3"
+  },
+  {
+    "revision": "da0da7c0b808b6040b77497fbe8f842a",
+    "url": "/media/SynthChime5.da0da7c0.mp3"
+  },
+  {
+    "revision": "35e3e377b25c7df70ea7295c3ea26bf5",
+    "url": "/media/Explosion6.35e3e377.mp3"
+  },
+  {
+    "revision": "2261f179c1d1ed5965c7a84a8b94ece2",
+    "url": "/media/UI_Quirky21.2261f179.mp3"
+  },
+  {
+    "revision": "89f7794a3e5ccdd0b16d3e7c5cf8718f",
+    "url": "/media/Bells9.89f7794a.mp3"
+  },
+  {
+    "revision": "8117f597879ac19d41b2b275a32a6ed2",
+    "url": "/media/Explosion2.8117f597.mp3"
+  },
+  {
+    "revision": "76ce28c73e55a8632528d68add19073d",
+    "url": "/media/flute1.76ce28c7.mp3"
+  },
+  {
+    "revision": "2b445e975910a1e8640966f421308188",
+    "url": "/media/Step.2b445e97.mp3"
+  },
+  {
+    "revision": "e9914d6b5163e959e339e868f5d185f3",
+    "url": "/media/UI_Quirky1.e9914d6b.mp3"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/fonts/MaterialIcons-Regular.c38ebd3c.woff"
+  },
+  {
+    "revision": "026dcd872a0ee0fbfb45",
+    "url": "/js/about.acc57fb1.js"
+  },
+  {
+    "revision": "55242ea5c378da626cab5b4aaf9c2255",
+    "url": "/fonts/MaterialIcons-Regular.55242ea5.ttf"
+  },
+  {
+    "revision": "7322e3b7887884edaa5e63545bddce4b",
+    "url": "/img/map.7322e3b7.png"
+  },
+  {
+    "revision": "0c485123a095633a5adc20101983617a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30b8c774cd635376f01afd3d21b229a7",
+    "url": "/img/map2.30b8c774.png"
+  },
+  {
+    "revision": "8c9590972fffc934fdf6365c010acab5",
+    "url": "/img/environment_forestbackground.8c959097.png"
+  },
+  {
+    "revision": "fd5d6b08169695dbcc19f085fe3bbde9",
+    "url": "/img/enemy1.fd5d6b08.png"
+  },
+  {
+    "revision": "7310b00c820e42548c392f6c471ab683",
+    "url": "/fonts/kenvector_future.7310b00c.ttf"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/fonts/MaterialIcons-Regular.8a9a261c.woff2"
+  },
+  {
+    "revision": "016c14adad95a0e21a2ae4fbc36955c6",
+    "url": "/fonts/MaterialIcons-Regular.016c14ad.eot"
+  },
+  {
+    "revision": "22a3b74b4aa635d966f4",
+    "url": "/css/chunk-vendors.9a79c881.css"
+  },
+  {
+    "revision": "e5233176773ba51e3417",
+    "url": "/css/app.653b681a.css"
+  }
+];
